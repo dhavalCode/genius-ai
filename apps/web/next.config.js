678165a9857ba6ab36c98b9cf4c64e43f5495b9d
Dhaval Patel <dhaval.contributor@gmail.com>
2023-10-08 00:01:06 +0530

    basic home page setup

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: [
+    "@genus-ai/prisma",
+    "@genus-ai/lib",
+    "@genus-ai/ui",
+  ],
 }
 
 module.exports = nextConfig
--- apps/web/src/pages/_document.tsx
+++ apps/web/src/pages/_document.tsx
@@ -1,13 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+import { Inter } from 'next/font/google'
+
+import { classNames } from "@genius-ai/lib/utils";
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" className={classNames("bg-secondary", inter.className)}>
       <Head />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
